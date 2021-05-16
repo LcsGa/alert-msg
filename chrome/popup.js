89071296abf78d24418f72199c1d174129b5c9e2
Lucas <lcs.grc@outlook.com>
2021-05-16 23:09:49 +0200

    feat: :sparkles: add a delete button

--- chrome/popup.js
+++ chrome/popup.js
@@ -1,36 +1,35 @@
 const button = document.querySelector("button");
 
 const sendAlert = () => {
-  return fetch("https://pixalert-e56b.restdb.io/rest/messages", {
-    method: "POST",
-    headers: {
-      "x-apikey": "60a0ef10e3b6e02545eda966",
-      "Content-Type": "application/JSON",
-    },
-    body: JSON.stringify({
-      message: "Connecte toi !",
-      timestamp: Date.now(),
-    }),
-  });
+    return fetch("https://pixalert-e56b.restdb.io/rest/messages", {
+        method: "POST",
+        headers: {
+        "x-apikey": "60a0ef10e3b6e02545eda966",
+        "Content-Type": "application/JSON",
+        },
+        body: JSON.stringify({
+        message: "Connecte toi !",
+        timestamp: Date.now(),
+        }),
+    });
 };
 
 button.addEventListener("click", async () => {
-  try {
-    const res = await sendAlert();
-    console.log(res);
-    if (res.status >= 200 && res.status < 300) {
-      button.classList.add("sent");
-    } else {
-      button.classList.add("not-sent");
+    try {
+        const res = await sendAlert();
+        if (res.status >= 200 && res.status < 300) {
+        button.classList.add("sent");
+        } else {
+        button.classList.add("not-sent");
+        }
+        setTimeout(() => {
+        button.removeAttribute("class");
+        }, 2000);
+    } catch (err) {
+        console.log(err);
+        button.classList.add("not-sent");
+        setTimeout(() => {
+        button.removeAttribute("class");
+        }, 2000);
     }
-    setTimeout(() => {
-      button.removeAttribute("class");
-    }, 2000);
-  } catch (err) {
-    console.log(err);
-    button.classList.add("not-sent");
-    setTimeout(() => {
-      button.removeAttribute("class");
-    }, 2000);
-  }
 });
--- js/index.js
+++ js/index.js
@@ -1,39 +1,64 @@
 const msgList = document.querySelector(".pix-message__list");
+const btnDelete = document.querySelector(".pix-message__button--delete");
 
 const getAllMsg = () => {
-  return fetch("https://pixalert-e56b.restdb.io/rest/messages", {
-    method: "GET",
-    headers: {
-      "x-apikey": "60a0ef10e3b6e02545eda966",
-    },
-  });
+    return fetch("https://pixalert-e56b.restdb.io/rest/messages", {
+        method: "GET",
+        headers: {
+            "x-apikey": "60a0ef10e3b6e02545eda966",
+        },
+    });
 };
+
+const deleteById = id => {
+    return fetch(`https://pixalert-e56b.restdb.io/rest/messages/${id}`, {
+        method: 'DELETE',
+        headers: {
+            "x-apikey": "60a0ef10e3b6e02545eda966",
+            "content-type": "application/json"
+        }
+    })
+}
 
 const displayMsgs = async () => {
-  try {
-    const messages = await (await getAllMsg()).json();
-
-    const msgItems = messages.map((msg) => {
-      const date = new Date(msg.timestamp);
-      const msgItem = document.createElement("li");
-      msgItem.classList.add("pix-message__message");
-      if (date.getUTCDate() == 18 && date.getHours() >= 9 && date.getMinutes() >= 10) {
-        msgItem.classList.add("pix-message__message--on-time");
-      }
-      msgItem.innerText = msg.message;
-      return msgItem;
-    });
-    msgList.innerHTML = "";
-    msgList.append(...msgItems);
-    const time = new Date();
-    console.clear();
-    console.log(`${time.toLocaleTimeString()}: Data fetched !`);
-  } catch (err) {
-    console.error(err);
-  }
+    try {
+        const messages = await (await getAllMsg()).json();
+        const msgItems = messages.map((msg) => {
+        const date = new Date(msg.timestamp);
+        const msgItem = document.createElement("li");
+        msgItem.classList.add("pix-message__message");
+        if (date.getUTCDate() == 18 && date.getHours() >= 9 && date.getMinutes() >= 10) {
+            msgItem.classList.add("pix-message__message--on-time");
+        }
+        msgItem.innerText = msg.message;
+        return msgItem;
+        });
+        msgList.innerHTML = "";
+        msgList.append(...msgItems);
+    } catch (err) {
+        console.error(err);
+    }
 };
 
+const deleteAll = async () => {
+    try {
+        const messages = await (await getAllMsg()).json();
+        messages.forEach(async msg => {
+            await deleteById(msg._id);
+        })
+        alert("Toutes les alertes ont été supprimées !");
+    } catch (err) {
+        console.error(err);
+    }
+}
+
 window.addEventListener("load", () => {
-  displayMsgs();
-  setInterval(displayMsgs, 5000);
+    displayMsgs();
+    setInterval(displayMsgs, 5000);
 });
+
+btnDelete.addEventListener("click", () => {
+    if (confirm("Etes-vous sûr de vouloir tout supprimer ?")) {
+        deleteAll();
+    }
+})
