394b1feae895ff189ab2daf21f652f7db6de8a4f
Lucas <lcs.grc@outlook.com>
2021-05-16 21:39:20 +0200

    feat: :lipstick: add a blue color to verify the date and time of the message

--- js/index.js
+++ js/index.js
@@ -1,34 +1,39 @@
 const msgList = document.querySelector(".pix-message__list");
 
 const getAllMsg = () => {
-    return fetch('https://pixalert-e56b.restdb.io/rest/messages', {
-        method: "GET",
-        headers: {
-            "x-apikey": "60a0ef10e3b6e02545eda966"
-        },
-    })
-}
+  return fetch("https://pixalert-e56b.restdb.io/rest/messages", {
+    method: "GET",
+    headers: {
+      "x-apikey": "60a0ef10e3b6e02545eda966",
+    },
+  });
+};
 
 const displayMsgs = async () => {
-    try {
-        const messages = await (await getAllMsg()).json();
-        const msgItems = messages.map(msg => {
-            const msgItem = document.createElement("li");
-            msgItem.classList.add("pix-message__message");
-            msgItem.innerText = msg.message;
-            return msgItem;
-        })
-        msgList.innerHTML = "";
-        msgList.append(...msgItems);
-        const time = new Date();
-        console.clear();
-        console.log( `${time.toLocaleTimeString()}: Data fetched !`);
-    } catch (err) {
-        console.error(err);
-    }
-}
+  try {
+    const messages = await (await getAllMsg()).json();
+
+    const msgItems = messages.map((msg) => {
+      const date = new Date(msg.timestamp);
+      const msgItem = document.createElement("li");
+      msgItem.classList.add("pix-message__message");
+      if (date.getUTCDate() == 18 && date.getHours() >= 9 && date.getMinutes() >= 10) {
+        msgItem.classList.add("pix-message__message--on-time");
+      }
+      msgItem.innerText = msg.message;
+      return msgItem;
+    });
+    msgList.innerHTML = "";
+    msgList.append(...msgItems);
+    const time = new Date();
+    console.clear();
+    console.log(`${time.toLocaleTimeString()}: Data fetched !`);
+  } catch (err) {
+    console.error(err);
+  }
+};
 
 window.addEventListener("load", () => {
-    displayMsgs();
-    setInterval(displayMsgs, 5000);
+  displayMsgs();
+  setInterval(displayMsgs, 5000);
 });
